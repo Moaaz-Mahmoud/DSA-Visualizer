9030b886987157afed613a7add10944ba316d21a
Moaaz Mahmoud <fasekhs@gmail.com>
2021-12-06 20:46:47 +0200

    Checkpoint

--- src/main/java/com/example/dsavisualizer/Controller.java
+++ src/main/java/com/example/dsavisualizer/Controller.java
@@ -1,18 +1,56 @@
 package com.example.dsavisualizer;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class Controller {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    /* Scene Setters
+     */
+    public void switchPage(ActionEvent event, String sceneFile) throws IOException {
+        root = FXMLLoader.load(getClass().getResource(sceneFile));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToLinkedListPage(ActionEvent event) throws IOException {
+        switchPage(event, "linked-list-page.fxml");
+    }
+    public void switchToStackPage(ActionEvent event) throws IOException {
+        switchPage(event, "stack-page.fxml");
+    }
+    public void switchToQueuePage(ActionEvent event) throws IOException{
+        switchPage(event, "queue-page.fxml");
+    }
+    public void switchToBSTPage(ActionEvent event) throws IOException{
+        switchPage(event, "bst-page.fxml");
+    }
+    public void switchToHeapPage(ActionEvent event) throws IOException{
+        switchPage(event, "heap-page.fxml");
+    }
+    public void switchToHashTablePage(ActionEvent event) throws IOException{
+        switchPage(event, "hash-table-page.fxml");
+    }
+
     /* General
      */
     @FXML
     private Label welcomeText;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    protected void onHelloButtonClick() {}
 
     /*
      */
