91ce59be639d05d2558ff87cf168b8230e9f709b
Moaaz Mahmoud <fasekhs@gmail.com>
2021-12-24 18:28:31 +0200

    Created the layout for the PerformanceAnalysis page.

--- src/main/java/com/example/dsavisualizer/PerformanceAnalysisPage.java
+++ src/main/java/com/example/dsavisualizer/PerformanceAnalysisPage.java
@@ -1,5 +1,51 @@
 package com.example.dsavisualizer;
 
-public class PerformanceAnalysisPage extends Controller{
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.*;
 
+public class PerformanceAnalysisPage extends Controller{
+    //Controls
+    //List
+    @FXML Button listInsertButton;@FXML Button listRemoveButton;
+    //Stack
+    @FXML ComboBox stackImplSpecifier;
+    @FXML Button stackInsertButton; @FXML Button stackRemoveButton;
+    //Queue
+    @FXML ComboBox queueImplSpecifier;
+    @FXML Button queueInsertButton; @FXML Button queueRemoveButton;
+    //Heap
+    @FXML Button heapInsertButton; @FXML Button heapRemoveButton;
+    //General
+    @FXML LineChart visualizationChart;
+
+    //Controller methods
+    //List
+    public void onListInsertButtonClick(){
+
+    }
+    public void onListRemoveButtonClick(){
+
+    }
+    //Stack
+    public void onStackInsertButtonClick(){
+
+    }
+    public void onStackRemoveButtonClick(){
+
+    }
+    //Queue
+    public void onQueueInsertButtonClick(){
+
+    }
+    public void onQueueRemoveButtonClick(){
+
+    }
+    //Heap
+    public void onHeapInsertButtonClick(){
+
+    }
+    public void onHeapRemoveButtonClick(){
+
+    }
 }
