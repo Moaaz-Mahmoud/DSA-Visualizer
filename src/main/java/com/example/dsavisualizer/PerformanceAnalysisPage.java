b2a80459faec67eeff86e32af0dfdac048a3efc3
Moaaz Mahmoud <fasekhs@gmail.com>
2021-12-24 19:28:53 +0200

    Created placeholder methods for the performance analysis controller.

--- src/main/java/com/example/dsavisualizer/PerformanceAnalysisPage.java
+++ src/main/java/com/example/dsavisualizer/PerformanceAnalysisPage.java
@@ -9,10 +9,10 @@ public class PerformanceAnalysisPage extends Controller{
     //List
     @FXML Button listInsertButton;@FXML Button listRemoveButton;
     //Stack
-    @FXML ComboBox stackImplSpecifier;
+    @FXML private ComboBox<String> stackImplSpecifier;
     @FXML Button stackInsertButton; @FXML Button stackRemoveButton;
     //Queue
-    @FXML ComboBox queueImplSpecifier;
+    @FXML private ComboBox<String> queueImplSpecifier;
     @FXML Button queueInsertButton; @FXML Button queueRemoveButton;
     //Heap
     @FXML Button heapInsertButton; @FXML Button heapRemoveButton;
@@ -22,30 +22,58 @@ public class PerformanceAnalysisPage extends Controller{
     //Controller methods
     //List
     public void onListInsertButtonClick(){
-
+        System.out.println("List");
     }
     public void onListRemoveButtonClick(){
-
+        System.out.println("List");
     }
     //Stack
     public void onStackInsertButtonClick(){
-
+        if(stackImplSpecifier.getValue().equals("Array-Based")){
+            System.out.println("Array-Based");
+        }
+        else if(stackImplSpecifier.getValue().equals("Link-Based")){
+            System.out.println("Link-Based");
+        }
     }
     public void onStackRemoveButtonClick(){
-
+        if(stackImplSpecifier.getValue().equals("Array-Based")){
+            System.out.println("Array-Based");
+        }
+        else if(stackImplSpecifier.getValue().equals("Link-Based")){
+            System.out.println("Link-Based");
+        }
     }
     //Queue
     public void onQueueInsertButtonClick(){
-
+        if(queueImplSpecifier.getValue().equals("Array-Based")){
+            System.out.println("Array-Based");
+        }
+        else if(queueImplSpecifier.getValue().equals("Link-Based")){
+            System.out.println("Link-Based");
+        }
     }
     public void onQueueRemoveButtonClick(){
-
+        if(queueImplSpecifier.getValue().equals("Array-Based")){
+            System.out.println("Array-Based");
+        }
+        else if(queueImplSpecifier.getValue().equals("Link-Based")){
+            System.out.println("Link-Based");
+        }
     }
     //Heap
     public void onHeapInsertButtonClick(){
-
+        System.out.println("Heap");
     }
     public void onHeapRemoveButtonClick(){
+        System.out.println("Heap");
+    }
+
+    //Other essential methods
+    public void initialize(){
+
+    }
+    public void render(){
 
     }
 }
